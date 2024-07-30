bd41b2e82bca56be6a6aaf1bab6913aed6fd2e11
Mitra USYD <mitraahmadi129@gmail.com>
2024-07-30 19:47:15 +1000

    adding file for role functions

--- routes/roles.js
+++ routes/roles.js
@@ -1,33 +1,5 @@
-const roles = require("express").Router();
+// const roles = require("express").Router();
 
-// FUNCTION TO FETCH THE ROLE DATA
-function viewAllRoles() {
-  roles.get("/api/roles", (req, res) => {
-    pool.query(
-      `SELECT 
-          role.title as "Role",
-          role.salary as "Salary",
-          department.name as "Department"
-        FROM 
-          role
-        JOIN 
-          department ON role.department_id = department.id`,
-      (err, data) => {
-        if (err) {
-          // FIX THIS UP LOOKS REALLY WEIRD
-          console.error(err);
-          res.status(500).json({ error: "An error occured" });
-          return;
-        }
-        console.table(data.rows);
-        res.json(data.rows);
-      }
-    );
-  });
-}
+// function updateEmployeeRole() {}
 
-function addRole() {}
-
-function updateEmployeeRole() {}
-
-module.exports = roles;
+// module.exports = roles;
