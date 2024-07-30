8f29a8cb99f669714db18e09235e3bc5198a89cf
Mitra USYD <mitraahmadi129@gmail.com>
2024-07-30 19:46:28 +1000

    adding index file to host functions for accessing database

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-const router = require("express").Router();
+// const router = require("express").Router();
 
-// Creating our separate route files into imported methods
-const employeeRouter = require("./employees");
-const departmentRouter = require("./departments");
-const roleRouter = require("./roles");
+// // Creating our separate route files into imported methods
+// const employeeRouter = require("./employees");
+// const departmentRouter = require("./departments");
+// const roleRouter = require("./roles");
 
-router.use("/employees", employeeRouter);
-router.use("/departments", departmentRouter);
-router.use("/roles", roleRouter);
+// router.use("/employees", employeeRouter);
+// router.use("/departments", departmentRouter);
+// router.use("/roles", roleRouter);
 
-module.exports = router;
+// module.exports = router;
